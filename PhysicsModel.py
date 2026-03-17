74575031bc1c9fb491067e7fb914c44d6ad26da4
AveryMunoz <averymunoz10@gmail.com>
2026-03-17 11:48:44 -0400

    Created threat missile trajectory draft

--- PhysicsModel.py
+++ PhysicsModel.py
@@ -6,27 +6,35 @@ def air_density(altitude):
     rho = rho_0 * (np.exp((-altitude)/H))
     return rho
 
-def forces(mass, velocity, altitude, max_thrust, Cd, A, H_t):
+def forces(mass, velocity, altitude, max_thrust, Cd, A, H_t, launch_angle, t):
     rho = air_density(altitude)
-    
-    g = 9.81
-    Cd = 0.3
-    missile_radius = 0.25
-    A = np.pi * missile_radius**2
-    H_t = 17500
+    vector_direction = np.array([
+            np.cos(launch_angle),
+            np.sin(launch_angle)
+        ])
     
     F_gravity  = np.array([0.0, -mass * 9.81])
+    gravity_mag = np.linalg.norm(F_gravity)
     
     velocity_mag = np.linalg.norm(velocity)
-    drag_mag = 0.5 * rho * velocity_mag**2 * Cd * A
-    F_drag = -drag_mag * (velocity / velocity_mag)
     
-    thrust_mag = max_thrust * np.exp(-altitude / H_t)
-    F_thrust = thrust_mag * (velocity / velocity_mag)
+    if velocity_mag == 0:
+        drag_mag = 0.5 * rho * velocity_mag**2 * Cd * A
+        F_drag = -drag_mag * vector_direction
+    else:
+        drag_mag = 0.5 * rho * velocity_mag**2 * Cd * A
+        F_drag = -drag_mag * (velocity / velocity_mag)
+    
+    if t > 120:  
+        thrust_mag = 0.0
+        F_thrust = thrust_mag * vector_direction
+    else:
+        thrust_mag = max_thrust * np.exp(-altitude / H_t)
+        F_thrust = thrust_mag * vector_direction
     
     tot_force = F_thrust + F_drag + F_gravity
     
-    return tot_force
+    return tot_force, F_thrust, F_drag, F_gravity, thrust_mag, drag_mag, gravity_mag
     
     
 def euler_integration(position, velocity, acceleration, dt):
---
+++ SimLoop.py
@@ -0,0 +1,58 @@
+from PhysicsModel import air_density, forces, euler_integration
+from Plot import trajectory_plot, forces_table
+import numpy as np
+
+# Defining missile constants (initial values that i could change later)
+mass = 1500
+g = 9.81
+Cd = 0.3
+missile_radius = 0.25
+A = np.pi * missile_radius**2
+H_t = 22500
+max_thrust = 75000
+
+# Time metrics
+dt = 1
+max_time = 1000
+
+# Launch parameters
+launch_angle = 60 * (np.pi / 180)
+launch_velocity = 0
+
+# Initial positions
+position = np.array([0.01, 0.01])
+velocity = np.array(
+    [launch_velocity * np.cos(launch_angle), 
+     launch_velocity * np.sin(launch_angle)]
+)
+
+# Create empty trajectory to store positions
+trajectory = []
+thrust_val = []
+gravity_val = []
+drag_val = []
+altitude_val = []
+time_val = []
+
+t = 0
+
+while position[1] >= 0 and t < max_time:
+    altitude = position[1]
+    F_total, F_thrust, F_drag, F_gravity, thrust_mag, drag_mag, gravity_mag = forces(mass, velocity, altitude, max_thrust, Cd, A, H_t, launch_angle, t)
+    acceleration  = F_total / mass
+    position, velocity = euler_integration(position, velocity, acceleration, dt)
+    trajectory.append(position.copy()) # .copy() is used in order to store updating frames of position
+    
+    thrust_val.append(F_thrust)
+    gravity_val.append(F_gravity)
+    drag_val.append(F_drag)
+    altitude_val.append(altitude)
+    time_val.append(t)
+    
+    t += dt
+
+trajectory = np.array(trajectory)
+# print(trajectory.shape)
+trajectory_plot(trajectory)
+forces_table(altitude_val, thrust_val, drag_val, gravity_val, time_val)
+
